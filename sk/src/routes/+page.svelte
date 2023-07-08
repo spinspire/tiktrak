bba1fc335679986dfe81a7131408f6005f4f5802
Jitesh Doshi <jitesh@spinspire.com>
2023-07-08 09:48:22 -0400

    bug fix: erroneous import

--- sk/src/routes/+page.svelte
+++ sk/src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { metadata } from "$lib/app/stores";
   import LoginGuard from "$lib/components/LoginGuard.svelte";
-  import List from "$lib/components/projects/List.svelte";
   $metadata.title = "Home";
 </script>
 
