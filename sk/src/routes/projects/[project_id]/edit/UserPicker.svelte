f3194f965b2f9c18f4108206a34da3db0e0e978f
Jitesh Doshi <jitesh@spinspire.com>
2023-07-09 08:00:18 -0400

    UserPicker: limit width

--- sk/src/routes/projects/[project_id]/edit/UserPicker.svelte
+++ sk/src/routes/projects/[project_id]/edit/UserPicker.svelte
@@ -8,29 +8,38 @@
 
 <Dialog>
   <button type="button" slot="trigger">Select Users</button>
-  <Paginator store={users} />
-  <table>
-    <tbody>
-      {#each $users.items as user}
-        <tr>
-          <td>
-            <label>
-              <input
-                type="checkbox"
-                name="uids"
-                bind:group={uids}
-                value={user.id}
-              />
-              {user.name || user.username}
-            </label>
-          </td>
-        </tr>
-      {:else}
-        <tr>
-          <td>No users found.</td>
-        </tr>
-      {/each}
-    </tbody>
-  </table>
-  <Paginator store={users} />
+  <div class="wrapper">
+    <h4>Select Users</h4>
+    <Paginator store={users} />
+    <table>
+      <tbody>
+        {#each $users.items as user}
+          <tr>
+            <td>
+              <label>
+                <input
+                  type="checkbox"
+                  name="uids"
+                  bind:group={uids}
+                  value={user.id}
+                />
+                {user.name || user.username}
+              </label>
+            </td>
+          </tr>
+        {:else}
+          <tr>
+            <td>No users found.</td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+    <Paginator store={users} />
+  </div>
 </Dialog>
+
+<style>
+  .wrapper {
+    max-width: 30em;
+  }
+</style>
