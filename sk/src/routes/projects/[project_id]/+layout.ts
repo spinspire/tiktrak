3ad26ee769ee6b87ebe8f4abb7b4d46251512a84
Jitesh Doshi <jitesh@spinspire.com>
2023-07-09 10:35:49 -0400

    bug fix: initialize project.users = []

--- sk/src/routes/projects/[project_id]/+layout.ts
+++ sk/src/routes/projects/[project_id]/+layout.ts
@@ -5,7 +5,7 @@ import type { LayoutLoad } from "./$types";
 export const load: LayoutLoad = async function ({ params: { project_id } }) {
   const project: ProjectsResponse =
     project_id === "new"
-      ? ({} as ProjectsResponse)
+      ? ({ users: [] } as ProjectsResponse)
       : await client
           .collection("projects")
           .getOne<ProjectsResponse>(project_id);
