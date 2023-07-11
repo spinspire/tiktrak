56fedea2a4e8ec5227af8547cd9fc913a30be241
Jitesh Doshi <jitesh@spinspire.com>
2023-07-11 12:49:05 -0400

    hide "new project" for non-admins

--- sk/src/routes/projects/+page.svelte
+++ sk/src/routes/projects/+page.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
   import { base } from "$app/paths";
   import { metadata } from "$lib/app/stores";
+  import LoginGuard from "$lib/components/LoginGuard.svelte";
   import type { PageData } from "./$types";
   export let data: PageData;
 
   $metadata.title = "Projects";
 </script>
 
-<a href="./new/edit/"><button type="button">new project</button></a>
+<LoginGuard adminOnly={true}>
+  <a href="./new/edit/"><button type="button">new project</button></a>
+</LoginGuard>
 {#each data.projects as item}
   <p><a href={`${base}/projects/${item.id}`}>{item.title}</a></p>
 {:else}
