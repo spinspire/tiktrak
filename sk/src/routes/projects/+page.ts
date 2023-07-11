a15b67d4647b049026129ec2236dc59410aef3af
Jitesh Doshi <jitesh@spinspire.com>
2023-07-11 12:59:09 -0400

    show alert when redirecting to sole project

--- sk/src/routes/projects/+page.ts
+++ sk/src/routes/projects/+page.ts
@@ -3,12 +3,14 @@ import { Admin } from "pocketbase";
 import type { PageLoad } from "./projects/$types";
 import { goto } from "$app/navigation";
 import { base } from "$app/paths";
+import { alerts } from "$lib/components/Alerts.svelte";
 
 export const load: PageLoad = async () => {
   const projects = await client.collection("projects").getFullList();
   // send non-admins to the project detail page if they have access to only one project
   if (projects.length === 1 && !(client.authStore.model instanceof Admin)) {
     const [project] = projects;
+    alerts.warning("Redirecting to your sole project.");
     goto(`${base}/projects/${project.id}`);
   }
   return {
