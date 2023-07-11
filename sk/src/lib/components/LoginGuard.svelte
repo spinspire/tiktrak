f414e6b7158b8ab449ef4bc5ff442461452a9c36
Jitesh Doshi <jitesh@spinspire.com>
2023-07-11 12:46:40 -0400

    LoginGuard: add prop - adminOnly

--- sk/src/lib/components/LoginGuard.svelte
+++ sk/src/lib/components/LoginGuard.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
+  import { Admin } from "pocketbase";
   import { authModel } from "../pocketbase";
   import LoginForm from "./LoginForm.svelte";
+  export let adminOnly = false;
   export let slotLogin = false;
   export let destination: string | null = null;
   $: if (destination != null && $authModel) {
@@ -9,7 +11,7 @@
   }
 </script>
 
-{#if $authModel}
+{#if $authModel && (!adminOnly || $authModel instanceof Admin)}
   <slot />
 {:else if slotLogin || $$slots["login"]}
   <slot name="login">
