0dc3fe4e7a4ee71e0a30d18221a9d3cd0495ce35
Jitesh Doshi <jitesh@spinspire.com>
2023-07-11 12:16:06 -0400

    handle project not found exception as HTTP 404

--- sk/src/routes/projects/[project_id]/+layout.ts
+++ sk/src/routes/projects/[project_id]/+layout.ts
@@ -1,16 +1,28 @@
 import { client } from "$lib/pocketbase";
 import type { ProjectsResponse } from "$lib/pocketbase/generated-types";
+import { ClientResponseError } from "pocketbase";
 import type { LayoutLoad } from "./$types";
+import { error } from "@sveltejs/kit";
 
 export const load: LayoutLoad = async function ({ params: { project_id } }) {
-  const project: ProjectsResponse =
-    project_id === "new"
-      ? ({ users: [] } as ProjectsResponse)
-      : await client
-          .collection("projects")
-          .getOne<ProjectsResponse>(project_id, { expand: "users" });
-
-  return {
-    project,
-  };
+  try {
+    const project: ProjectsResponse =
+      project_id === "new"
+        ? ({ users: [] } as ProjectsResponse)
+        : await client
+            .collection("projects")
+            .getOne<ProjectsResponse>(project_id, { expand: "users" });
+    return {
+      project,
+    };
+  } catch (e) {
+    if (e instanceof ClientResponseError && e.status === 404) {
+      // replace ClientResponseError with HttpError
+      throw error(
+        404,
+        "Project not found. Make sure it exists and you have access."
+      );
+    }
+    throw e; // else just rethrow
+  }
 };
