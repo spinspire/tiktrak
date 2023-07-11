b689c8b89908a64e41d5a81228a614622eab4913
Jitesh Doshi <jitesh@spinspire.com>
2023-07-11 12:17:34 -0400

    send user to the sole project they have access to

--- sk/src/routes/projects/+page.ts
+++ sk/src/routes/projects/+page.ts
@@ -1,8 +1,16 @@
 import { client } from "$lib/pocketbase";
+import { Admin } from "pocketbase";
 import type { PageLoad } from "./projects/$types";
+import { goto } from "$app/navigation";
+import { base } from "$app/paths";
 
 export const load: PageLoad = async () => {
   const projects = await client.collection("projects").getFullList();
+  // send non-admins to the project detail page if they have access to only one project
+  if (projects.length === 1 && !(client.authStore.model instanceof Admin)) {
+    const [project] = projects;
+    goto(`${base}/projects/${project.id}`);
+  }
   return {
     projects,
   };
