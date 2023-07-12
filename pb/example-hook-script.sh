aa495a82fe7afe04f3c09eee271505f4612d1470
Jitesh Doshi <jitesh@spinspire.com>
2023-07-12 19:11:42 -0400

    improve example-hook-script.sh

--- pb/example-hook-script.sh
+++ pb/example-hook-script.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # This example script, along with hooks.go, shows how to trigger a command
 # when a record changes in PocketBase and how to feed the changed record to this
@@ -10,3 +10,8 @@ echo "PARAMS=$params"
 # The body of the record (as JSON) is fed to this script as stdin.
 # The following just reformats it and pretty-prints it.
 cat | jq -C
+
+# example: transform incoming JSON
+#json=$(cat | jq -r '{message: "ticket updated: \(.title); in project \(.expand.project.title)"}')
+# post it somewhere
+#curl -s --data "$json" -H 'Content-Type: application/json' $webhook_url
